74a3d1e9203c5b450439d8ee84383a2521d1225a
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-01 01:28:03 +0800

    axios封装，及数据请求

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,11 +8,27 @@
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultiData} from "@/network/home";
 
 export default {
   name: "Home",
   components: {
     NavBar
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    //初始化请求多个数据
+    getHomeMultiData().then(res => {
+      this.banners = res.data.banner
+      this.recommends = res.data.recommend
+    }).catch(err => {
+      console.log(err);
+    })
   }
 }
 </script>
