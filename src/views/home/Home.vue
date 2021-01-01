53979857e0296a2847854ea3f7a3daf0a5e37e90
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-01 21:11:45 +0800

    集成better scroll

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,63 +1,13 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
-    <swiper :items="banners.list"></swiper>
-    <recommend-view :recommends="recommends.list"/>
-    <feature-view/>
-    <tab-control class="tab-control" :titles="titles" @itemClick="tabClick"/>
-    <goods-list :goods="showGoods"/>
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-    </ul>
+    <scroll class="content">
+      <swiper :items="banners.list"></swiper>
+      <recommend-view :recommends="recommends.list"/>
+      <feature-view/>
+      <tab-control class="tab-control" :titles="titles" @itemClick="tabClick"/>
+      <goods-list :goods="showGoods"/>
+    </scroll>
   </div>
 </template>
 
@@ -66,6 +16,7 @@ import NavBar from "@/components/common/navbar/NavBar";
 import Swiper from "@/components/common/swiper/Swiper";
 import TabControl from "@/components/content/tabControl/TabControl";
 import GoodsList from "@/components/content/goods/GoodsList";
+import Scroll from "@/components/common/scroll/Scroll";
 
 import RecommendView from "@/views/home/childComps/RecommendView";
 import FeatureView from "@/views/home/childComps/FeatureView";
@@ -80,7 +31,8 @@ export default {
     Swiper,
     RecommendView,
     FeatureView,
-    GoodsList
+    GoodsList,
+    Scroll
   },
   data() {
     return {
@@ -146,6 +98,7 @@ export default {
 <style scoped>
 #home {
   padding-top: 44px;
+  height: 100vh;
 }
 
 .home-nav {
@@ -158,5 +111,15 @@ export default {
   position: sticky;
   top: 44px;
   z-index: 5;
+}
+
+.content {
+  height: calc(100% - 94px);
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
 }
 </style>
